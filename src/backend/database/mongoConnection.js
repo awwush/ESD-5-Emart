eaa0f40a2ac5c6dde803b7e63367459ede73f73b
Pratik <pratiktiwari2001@gmail.com>
2021-03-05 16:28:24 +0530

    home running pratik

--- src/backend/database/mongoConnection.js
+++ src/backend/database/mongoConnection.js
@@ -6,11 +6,10 @@
 // .catch((error) => console.log(error))
 
 
-
 // online mongodb connection
 
 const mongoose = require("mongoose");
-var connectionUrl = " mongodb+srv://admin:<ask on whatsapp>@cluster0.topdd.mongodb.net/shop?retryWrites=true&w=majority";
+var connectionUrl = " mongodb+srv://admin:1234@cluster0.topdd.mongodb.net/shop?retryWrites=true&w=majority";
 
 mongoose.connect(connectionUrl, {useNewUrlParser : true, useUnifiedTopology : true})
 .then(() => console.log("connected to database successfully"))
